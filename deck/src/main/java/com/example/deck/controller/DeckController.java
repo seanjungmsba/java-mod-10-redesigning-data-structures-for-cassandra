7907002a0915929ef947658a5536c07a8d659cd0
seanjungmsba <seanjeong1996@gmail.com>
2022-08-31 15:55:38 -0400

    modified deck controller

--- deck/src/main/java/com/example/deck/controller/DeckController.java
+++ deck/src/main/java/com/example/deck/controller/DeckController.java
@@ -6,11 +6,11 @@ import com.example.deck.model.Value;
 import com.example.deck.repository.CardRepository;
 import com.example.deck.repository.DeckRepository;
 import com.example.deck.repository.ValueRepository;
+import com.example.deck.service.ValueService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.*;
 
@@ -18,6 +18,8 @@ import static java.util.UUID.randomUUID;
 
 @RestController
 public class DeckController {
+
+    private Long positionCounter = 1L;
 
     @Autowired
     private DeckRepository deckRepository;
@@ -28,9 +30,12 @@ public class DeckController {
     @Autowired
     private ValueRepository valueRepository;
 
+    @Autowired
+    private ValueService valueService;
+
     @GetMapping("/new")
     public String newDeck(@RequestParam(value = "decks", defaultValue = "1") Long decks) {
-        
+
         // Drop tables and start new
         deckRepository.deleteAll();
         cardRepository.deleteAll();
@@ -53,13 +58,17 @@ public class DeckController {
 
         // Initialize card table
         List<String> suits = Arrays.asList("Clubs", "Hearts", "Spades", "Diamonds");
-        List<String> names = Arrays.asList("Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
-                "Ten", "Jack", "Queen", "King", "Ace");
+        List<String> names = Arrays.asList("Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+                "Nine", "Ten", "Jack", "Queen", "King", "Ace");
+
         for (Long deck = 1L; deck <= decks; deck++) {
             for (String suit : suits) {
                 for (String name : names) {
                     UUID uuid = randomUUID();
-                    cardRepository.save(new Card(uuid, name, suit, deck));
+                    Card card = new Card(uuid, name, suit, deck);
+                    Value value = valueService.getValueByName(name);
+                    card.setPoints(value.getPoints());
+                    cardRepository.save(card);
                 }
             }
         }
@@ -72,7 +81,6 @@ public class DeckController {
             deckRepository.save(deckItem);
             position++;
         }
-
         return String.format("New Deck using %s decks.", decks);
     }
 
@@ -101,11 +109,12 @@ public class DeckController {
     @GetMapping("/deal")
     public String dealCard() {
 
-        Deck deckItem = deckRepository.findFirstByOrderByPositionDesc().orElseGet(null);
+        Deck deckItem = deckRepository.findByPosition(positionCounter++).orElseGet(null);
         deckRepository.delete(deckItem);
 
-        return String.format("Dealt %s of %s: Worth %s points.", deckItem.getCard().getName(), deckItem.getCard().getSuit(),
-                            deckItem.getCard().getValue().getPoints());
+        return String.format("Dealt %s of %s: Worth %s points.", deckItem.getCardName(),
+                deckItem.getSuit(),
+                deckItem.getPoints());
     }
 
 }
